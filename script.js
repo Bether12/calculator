d0a371b72701bd8c0fd6cfbf52e8143e67874000
Bether12 <elnenogomez13@gmail.com>
2025-11-28 07:08:19 -0500

    Define operate() function to decide which operation to do based on the operator provided

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function divide(num1, num2){
 let number1 = 0;
 let number2 = 0;
 let operator = '';
+
+function operate(number1, number2, operator){
+    switch(operator){
+        case '+':
+            return add(number1,number2);
+        case '-':
+            return subtract(number1, number2);
+        case '*':
+            return multiply(number1, number2);
+        case '/':
+            return divide(number1, number2);
+        default:
+            return 'Operator not supported';
+    }
+}
